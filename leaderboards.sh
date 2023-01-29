4e905f4a3a449917e0587e3039ce698f148d4506
Ed Bardsley <ebardsley@gmail.com>
2023-01-29 02:08:06 -0800

    Use glob for pson files.

--- leaderboards.sh
+++ leaderboards.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 pushd ../nytxw/
 if [[ -z "${AGG_ONLY:-}" ]]; then ./leaderboards.sh; fi
 
-direnv exec . ./aggregate.py ../nyt-leaderboard-fe/src/nytxw-combined.json *.pson
+direnv exec . ./aggregate.py ../nyt-leaderboard-fe/src/nytxw-combined.json 'data/**/*.pson'
 
 popd
 npm run build
