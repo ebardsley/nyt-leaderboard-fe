c025478974c716ef3f702fd16af1e5a40fbbf4f8
Ed Bardsley <ebardsley@gmail.com>
2024-07-04 00:01:27 -0700

    leaderboards: pull from a syncthing path too

--- leaderboards.sh
+++ leaderboards.sh
@@ -5,7 +5,11 @@ set -e
 cd "$(dirname "$0")"
 pushd ../nytxw/
   if [[ -z "${AGG_ONLY:-}" ]]; then ./leaderboards.sh; fi
-  direnv exec . ./aggregate.py ../nyt-leaderboard-fe/src/nytxw-combined.json 'data/**/*.pson'
+  sources=('data/**/*.pson')
+  if id syncthing > /dev/null 2>&1; then
+    sources+=($(getent passwd syncthing | cut -d: -f6)'/nytxw-manual/*.pson')
+  fi
+  direnv exec . ./aggregate.py ../nyt-leaderboard-fe/src/nytxw-combined.json "${sources[@]}"
 popd
 
 # To setup fresh node_modules, run the same command w/ "npm install".
