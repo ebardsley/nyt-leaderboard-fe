bac883653758d0605e9a5a8227049bd2caf157d0
Ed Bardsley <ebardsley@gmail.com>
2024-10-07 22:41:39 -0700

    leaderboards: get data from sqlite

--- leaderboards.sh
+++ leaderboards.sh
@@ -5,11 +5,7 @@ set -e
 cd "$(dirname "$0")"
 pushd ../nytxw/
   if [[ -z "${AGG_ONLY:-}" ]]; then ./leaderboards.sh; fi
-  sources=('data/**/*.pson')
-  if id syncthing > /dev/null 2>&1; then
-    sources+=($(getent passwd syncthing | cut -d: -f6)'/nytxw-manual/*.pson')
-  fi
-  direnv exec . ./aggregate.py ../nyt-leaderboard-fe/src/nytxw-combined.json "${sources[@]}"
+  direnv exec . ./sqlite_to_json.py data/mini.sqlite3 ../nyt-leaderboard-fe/src/nytxw-combined.json
 popd
 
 # To setup fresh node_modules, run the same command w/ "npm install".
